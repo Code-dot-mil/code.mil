2a099fa5f3518cafc35c0fc5ad8f767e06e4ee1a
Jordan Kasper <jordan@dds.mil>
2017-12-18 14:06:56 -0500

    Updated FAQ page with sidebar nav, including dynamic generation of sidenav items

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,18 @@
 // less official: "The .gov means it's official."
 let govBanner = document.querySelector('.usa-banner-guidance-gov strong');
 govBanner.innerText = govBanner.innerText.replace(/\.gov/, '.mil');
+
+if (window.location.pathname === '/frequently-asked-questions') {
+  var list = document.querySelector('.usa-sidenav-sub_list');
+
+  if (list) {
+    Array.prototype.concat.apply([], document.querySelectorAll('#main-content h1'))
+      .forEach(function(node, i) {
+        if (i === 0) { return; }  // We manual add the first FAQ because otherwise the subnav ul won't exist
+
+        var item = document.createElement('li');
+        item.innerHTML = '<a href="/frequently-asked-questions#' + node.getAttribute('id') + '">' + node.innerText + '</a>';
+        list.appendChild(item);
+      });
+  }
+}
